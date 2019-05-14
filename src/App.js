3356695ab7c46a2b19cc0826c447e188ec69a58c
Sara Coren <saracoren@Saras-MacBook-Air.local>
2019-05-14 17:07:40 -0400

    committing so I can pull changes

--- src/App.js
+++ src/App.js
@@ -1,102 +1,322 @@
-import React from 'react';
-import './App.css';
-import NewForm from './components/NewForm'
-import BucketLogo from "./BucketLogo.png"
+import React from "react";
+// import reactDOM from "react-dom";
+import "./App.css"
+import TransitionGroup from "react-transition-group";
 
-let baseURL = process.env.REACT_APP_BASEURL
-//alternate baseURL = 'https://fathomless-sierra-68956.herokuapp.com'
-if (process.env.NODE_ENV === 'development') {
-  baseURL = 'http://localhost:3003'
-} else {
-  baseURL = 'https://dry-river-83879.herokuapp.com'
-}
-
-
-
+// import FadeTransition from "../src/transitions/fadeTransition";
 
 class App extends React.Component {
-constructor(props) {
-  super(props)
-  this.state = {
-    adventures: []
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoginOpen: true,
+      isRegisterOpen: false
+    };
   }
-  this.getAdventures = this.getAdventures.bind(this)
-  this.deleteAdventure = this.deleteAdventure.bind(this)
- this.handleAddAdventure = this.handleAddAdventure.bind(this)
-}
-componentDidMount(){
-  this.getAdventures()
-}
-deleteAdventure(id) {
-  fetch(baseURL + '/adventures/' + id, {
-    method: 'DELETE'
-  }).then(response => {
-    const findIndex = this.state.adventures.findIndex(adventure => adventure._id === id)
-    const copyAdventures = [...this.state.adventures]
-    copyAdventures.splice(findIndex, 1)
-    this.setState({ adventures: copyAdventures })
-  })
-}
 
-getAdventures () {
-  fetch(baseURL+ '/adventures')
-  .then(data => {
-    return data.json()},
-    err=> console.log(err))
-    .then(parsedData => {
-      //console.log(parsedData)
-       this.setState({adventures:parsedData})
-  },
-  err=> console.log(err))
-} 
-handleAddAdventure(adventures) {
-  const copyAdventures = [...this.state.adventures]
-  console.log(copyAdventures)
-  copyAdventures.unshift(adventures)
-  console.log(copyAdventures)
-  this.setState({
-    adventures: copyAdventures,
-    title: '',
-    img: ''
-  })
-}
+  showLoginBox() {
+    this.setState({isLoginOpen: true, isRegisterOpen: false});
+  }
+
+  showRegisterBox() {
+    this.setState({isRegisterOpen: true, isLoginOpen: false});
+  }
+
   render() {
-console.log(baseURL)
-    return (
-      
-      <div className="App">
-      <img className="logo" src={BucketLogo} alt="Logo" />
-      <div className="form-container">
-      <h1>Bucket List Adventures</h1>
-      <h4>Stop wishing. Start living!</h4>
-       <NewForm handleAddAdventure={this.handleAddAdventure}/>
-        </div>
-<div className='container'> 
-{this.state.adventures.map(adventure => {
-  console.log(this.state.adventures)
-  return (
-    
-<div className="card" key={adventure._id} >
-<div className="top-div"> {adventure.title}</div>
-<div>
-<img src={adventure.img} alt='image' height='300'/>
-{/* <li>{adventure.notes}</li> */}
-{/* <li>{adventure.completed}</li> */}
-</div>
-<div className="bottom-div">
-<span className="edit" >Edit</span>
-<span className="delete" onClick={() => this.deleteAdventure(adventure._id)}>Delete</span>
-</div>
-</div>
 
-  )
-})}
-</div>
+    return (
+      <div className="root-container">
+
+        <div className="box-controller">
+          <div
+            className={"controller " + (this.state.isLoginOpen
+            ? "selected-controller"
+            : "")}
+            onClick={this
+            .showLoginBox
+            .bind(this)}>
+            Login
+          </div>
+          <div
+            className={"controller " + (this.state.isRegisterOpen
+            ? "selected-controller"
+            : "")}
+            onClick={this
+            .showRegisterBox
+            .bind(this)}>
+            Register
+          </div>
+        </div>
+ 
+        <FadeTransition isOpen={this.state.isLoginOpen} duration={500}>
+          <div className="box-container">
+            <LoginBox/>
+          </div>
+        </FadeTransition>
+        <FadeTransition isOpen={this.state.isRegisterOpen} duration={500}>
+          <div className="box-container">
+            <RegisterBox/>
+          </div>
+        </FadeTransition>
 
       </div>
     );
 
   }
+
+}
+
+class LoginBox extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
   }
+
+  submitLogin(e) {}
+
+  render() {
+    return (
+      <div className="inner-container">
+        <div className="header">
+          Login
+        </div>
+        <div className="box">
+
+          <div className="input-group">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              name="username"
+              className="login-input"
+              placeholder="Username"/>
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              className="login-input"
+              placeholder="Password"/>
+          </div>
+
+          <button
+            type="button"
+            className="login-btn"
+            onClick={this
+            .submitLogin
+            .bind(this)}>Login</button>
+
+        </div>
+      </div>
+    );
+  }
+
+}
+
+function pop(props) {
+  return
+}
+
+class RegisterBox extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      email: "",
+      password: "",
+      errors: [],
+      pwdState: null
+    };
+  }
+
+  showValidationErr(elm, msg) {
+    this.setState((prevState) => ({
+      errors: [
+        ...prevState.errors, {
+          elm,
+          msg
+        }
+      ]
+    }));
+  }
+
+  clearValidationErr(elm) {
+    this.setState((prevState) => {
+      let newArr = [];
+      for (let err of prevState.errors) {
+        if (elm != err.elm) {
+          newArr.push(err);
+        }
+      }
+      return {errors: newArr};
+    });
+  }
+
+  onUsernameChange(e) {
+    this.setState({username: e.target.value});
+    this.clearValidationErr("username");
+  }
+
+  onEmailChange(e) {
+    this.setState({email: e.target.value});
+    this.clearValidationErr("email");
+  }
+
+  onPasswordChange(e) {
+    this.setState({password: e.target.value});
+    this.clearValidationErr("password");
+
+    this.setState({pwdState: "weak"});
+    if (e.target.value.length > 8) {
+      this.setState({pwdState: "medium"});
+    } else if (e.target.value.length > 12) {
+      this.setState({pwdState: "strong"});
+    }
+
+  }
+
+  openPopup(e) {
+    console.log("Hello world!");
+  }
+
+  submitRegister(e) {
+
+    console.log(this.state);
+
+    if (this.state.username == "") {
+      this.showValidationErr("username", "Username Cannot be empty!");
+    }
+    if (this.state.email == "") {
+      this.showValidationErr("email", "Email Cannot be empty!");
+    }
+    if (this.state.password == "") {
+      this.showValidationErr("password", "Password Cannot be empty!");
+    }
+
+  }
+
+  render() {
+
+    let usernameErr = null,
+      passwordErr = null,
+      emailErr = null;
+
+    for (let err of this.state.errors) {
+      if (err.elm == "username") {
+        usernameErr = err.msg;
+      }
+      if (err.elm == "password") {
+        passwordErr = err.msg;
+      }
+      if (err.elm == "email") {
+        emailErr = err.msg;
+      }
+    }
+
+    let pwdWeak = false,
+      pwdMedium = false,
+      pwdStrong = false;
+
+    if (this.state.pwdState == "weak") {
+      pwdWeak = true;
+    } else if (this.state.pwdState == "medium") {
+      pwdWeak = true;
+      pwdMedium = true;
+    } else if (this.state.pwdState == "strong") {
+      pwdWeak = true;
+      pwdMedium = true;
+      pwdStrong = true;
+    }
+
+    return (
+      <div className="inner-container">
+        <div className="header">
+          Register
+        </div>
+        <div className="box">
+
+          <div className="input-group">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              name="username"
+              className="login-input"
+              placeholder="Username"
+              onChange={this
+              .onUsernameChange
+              .bind(this)}/>
+            <small className="danger-error">{usernameErr
+                ? usernameErr
+                : ""}</small>
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              name="email"
+              className="login-input"
+              placeholder="Email"
+              onChange={this
+              .onEmailChange
+              .bind(this)}/>
+            <small className="danger-error">{emailErr
+                ? emailErr
+                : ""}</small>
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              className="login-input"
+              placeholder="Password"
+              onChange={this
+              .onPasswordChange
+              .bind(this)}/>
+            <small className="danger-error">{passwordErr
+                ? passwordErr
+                : ""}</small>
+
+            {this.state.password && <div className="password-state">
+              <div
+                className={"pwd pwd-weak " + (pwdWeak
+                ? "show"
+                : "")}></div>
+              <div
+                className={"pwd pwd-medium " + (pwdMedium
+                ? "show"
+                : "")}></div>
+              <div
+                className={"pwd pwd-strong " + (pwdStrong
+                ? "show"
+                : "")}></div>
+            </div>}
+
+          </div>
+
+          <button
+            type="button"
+            className="login-btn"
+            onHover={this
+            .openPopup
+            .bind(this)}
+            onClick={this
+            .openPopup
+            .bind(this)}>Register</button>
+
+        </div>
+      </div>
+
+    );
+
+  }
+
+}
 
 export default App;
